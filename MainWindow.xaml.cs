dfc8cbd2c2401402abe4e74b590cd788c813b3ad
viktorcpp <viktor.cpp.root@gmail.com>
2020-06-12 11:38:00 +0300

    First commit after repo restoring

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Management;
+
+namespace WinClocks
+{
+    public partial class MainWindow:Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += OnLoad;
+            this.Deactivated += OnDeactivate;
+
+        } // MainWindow
+
+        private void OnDeactivate(object o, EventArgs e)
+        {
+            this.Topmost = true;
+
+        } // OnDeactivate
+
+        private void OnLoad(object o, EventArgs e)
+        {
+            HwndSource source = HwndSource.FromHwnd( new WindowInteropHelper(this).Handle );
+            source.AddHook( new HwndSourceHook(WndProc) );
+
+            this.MouseLeftButtonDown += OnMouseDown;
+
+            MWinAPI.SetTimer( new WindowInteropHelper(this).Handle, 10, 10, IntPtr.Zero );
+
+            int offset = 0;
+            this.Left  = 0;
+            this.Top   = 0;
+
+            for( int x = 0; x < System.Windows.Forms.Screen.AllScreens.Length; x++ )
+            {
+                System.Drawing.Rectangle rect = System.Windows.Forms.Screen.AllScreens[x].Bounds;
+                offset += rect.Width;
+            }
+
+            this.Left = offset - this.Width;
+            
+        } // OnLoad
+
+        private void OnMouseDown( object sender,MouseButtonEventArgs e )
+        {
+            this.DragMove();
+            
+        } // OnMouseDown
+
+        private static IntPtr WndProc( IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled )
+        {
+            MainWindow mwin = (MainWindow)Application.Current.MainWindow;
+
+            if( msg == MWinAPI.WM_ACTIVATE )
+            {
+                mwin.Topmost = true;
+            }
+            if( msg == MWinAPI.WM_TIMER )
+            {
+                string hours = DateTime.Now.TimeOfDay.Hours   < 10 ? "0" + DateTime.Now.TimeOfDay.Hours   : DateTime.Now.TimeOfDay.Hours   + "";
+                string min   = DateTime.Now.TimeOfDay.Minutes < 10 ? "0" + DateTime.Now.TimeOfDay.Minutes : DateTime.Now.TimeOfDay.Minutes + "";
+                string sec   = DateTime.Now.TimeOfDay.Seconds < 10 ? "0" + DateTime.Now.TimeOfDay.Seconds : DateTime.Now.TimeOfDay.Seconds + "";
+
+                string mon = DateTime.Now.Month < 10 ? "0" + DateTime.Now.Month : DateTime.Now.Month + "";
+                string day = DateTime.Now.Day   < 10 ? "0" + DateTime.Now.Day   : DateTime.Now.Day   + "";
+                string yer = DateTime.Now.Year  < 10 ? "0" + DateTime.Now.Year  : DateTime.Now.Year  + "";
+
+                mwin.lblTime.Content = string.Format( "{0}:{1}:{2}", hours, min, sec );
+                mwin.lblDay .Content = DateTime.Now.DayOfWeek.ToString().ToUpper();
+                mwin.lblDate.Content = string.Format("{0}.{1}.{2}", mon, day, yer);
+            }
+            
+            return IntPtr.Zero;
+
+        } // IntPtr WndProc
+
+    } // class MainWindow
+
+} // namespace WinClocks
---
+++ WinAPI.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinClocks
+{
+    class MWinAPI
+    {
+        public const uint SHOWWINDOW = 0x0040;
+
+        public const int  GWL_STYLE   = -16;
+        public const int  GWL_EXSTYLE = -20;
+
+        public const long WS_POPUP    = 0x80000000L;
+        public const long WS_BORDER   = 0x00800000; //window with border
+        public const long WS_DLGFRAME = 0x00400000; //window with double border but no title
+        public const long WS_CAPTION  = WS_BORDER | WS_DLGFRAME; //window with a title bar 
+
+        public const int  WM_KEYDOWN     = 0x0100;
+        public const int  WM_ACTIVATE    = 0x0006;
+        public const int  WM_TIMER       = 0x0113;
+
+        public const int  WA_ACTIVE      = 1;
+        public const int  WA_CLICKACTIVE = 2;
+        public const int  WA_INACTIVE    = 0;
+
+
+        public const int  VK_F7       = 0x76;
+        public const int  VK_F8       = 0x77;
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE            = (0x0001),
+            SUSPEND_RESUME       = (0x0002),
+            GET_CONTEXT          = (0x0008),
+            SET_CONTEXT          = (0x0010),
+            SET_INFORMATION      = (0x0020),
+            QUERY_INFORMATION    = (0x0040),
+            SET_THREAD_TOKEN     = (0x0080),
+            IMPERSONATE          = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport( "user32.dll" )]
+        internal static extern bool MoveWindow( IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint );
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong( IntPtr hWnd, int nIndex, long dwNewLong );
+
+        [DllImport("user32.dll")]
+        public static extern long GetWindowLong( IntPtr hWnd, int nIndex );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread( MWinAPI.ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId );
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread( IntPtr hThread );
+
+        [DllImport("kernel32.dll")]
+        public static extern int ResumeThread( IntPtr hThread );
+
+        [DllImport("user32.dll")]
+        public static extern void ClipCursor( IntPtr rect );
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect( IntPtr hWnd, out IntPtr rect );
+
+        //System.Drawing.Rectangle r = new System.Drawing.Rectangle(10, 10, 500, 500);
+        //ClipCursor(ref r);
+        //ClipCursor(IntPtr.Zero);\
+        [DllImport("user32.dll")]
+        public static extern uint SetTimer( IntPtr hWnd, uint iTimerId, uint timeout, IntPtr func );
+
+    } // class MWinAPI
+
+} // namespace WinClocks
